5e8844d46061f4d4ec85d145d82a2e414f725c08
Sanket9831 <Sanketbunty9831@gmail.com>
2020-04-13 18:38:58 +0530

    Performed Step 1

---
+++ Mini_project_1.py
@@ -0,0 +1,11 @@
+# Print the Game Instruction to the User/Player 
+print("Winning Rules of the Rock Paper Scissor Game as follows: \n"
+                                +"\tRock  vs Paper   -> Paper wins \n"
+                                +"\tRock  vs Scissor -> Rock wins \n"
+                                +"\tPaper vs Scissor -> Scissor wins \n") 
+
+
+
+
+# Print a Thanks Message to the user
+print("\nThanks for playing Rock Paper Scissor Game \n") 
