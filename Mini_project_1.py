209c265218ec84de6c8700cc1b39af7a4bcc0ceb
Sanket9831 <Sanketbunty9831@gmail.com>
2020-04-13 18:42:53 +0530

    Performed Step 2

--- Mini_project_1.py
+++ Mini_project_1.py
@@ -1,11 +1,21 @@
 # Print the Game Instruction to the User/Player 
 print("Winning Rules of the Rock Paper Scissor Game as follows: \n"
-                                +"\tRock  vs Paper   -> Paper wins \n"
-                                +"\tRock  vs Scissor -> Rock wins \n"
-                                +"\tPaper vs Scissor -> Scissor wins \n") 
+                                +"\tRock  vs paper   -> paper wins \n"
+                                +"\tRock  vs scissor -> Rock wins \n"
+                                +"\tpaper vs scissor -> scissor wins \n") 
+
+while True:
 
 
-
-
+    
+    # Get the response from User to Play Again    
+    ans = input("Do you want to play again ? (Y/N) > ") 
+  
+    # Check the response from User and continue the loop
+    # Othewise break the loop to close the game
+    if (ans == 'n' or ans == 'N') : 
+        break
+    
+    
 # Print a Thanks Message to the user
 print("\nThanks for playing Rock Paper Scissor Game \n") 
